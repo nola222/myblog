1562b47db278e5035b31519117672e2d7beff42e
lixianchun <lixianchun@cyai.com>
2020-09-09 16:54:42 +0800

    add():urls

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('account.urls')),  # 用户注册和登录
+    path('', include('article.urls')),  # 博客文章
+    path('album/', include('album.urls')),  # 照片墙
+    path('board/', include('interflow.urls')),  # 留言板
+    re_path('media/(?P<path>.*)', serve,
+            {'document_root': settings.MEDIA_ROOT}, name='media'),  # 配置媒体资源
 ]
